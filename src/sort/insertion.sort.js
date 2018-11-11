4cf3fae13dc5ee1991a4fa0afe7f76408ca232d2
narekhovhannisyan <narekgilmour@gmail.com>
2018-11-11 20:07:27 +0400

    src(sort): implement insertion sort in insertion.sort.js

---
+++ src/sort/insertion.sort.js
@@ -0,0 +1,25 @@
+/**
+ * Sorts given `array` using insertion sort algorithm.
+ * @param array {Array}
+ * @returns {Array}
+ */
+const insertionSort = (array) => {
+  const arrayLength = array.length
+
+  let i = 1
+  for (i; i < arrayLength; i++) {
+
+    let j = i
+    while (j >= 0 && array[j - 1] > array[j]) {
+      array[j] = array[j] + array[j - 1]
+      array[j - 1] = array[j] - array[j - 1]
+      array[j] = array[j] - array[j - 1]
+
+      j--
+    }
+  }
+
+  return array
+}
+
+module.exports = insertionSort;
